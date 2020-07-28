f437ff7140f3f670a7b87378a393b8588543e358
Muna Kaurimuje <muna.kaurimuje@gmail.com>
2020-07-28 21:00:33 +0200

    Initial Code

---
+++ lib/models/article_model.dart
@@ -0,0 +1,13 @@
+class ArticleModel{
+
+  String author;
+  String title;
+  String description;
+  String url;
+  String urlToImage;
+  String content;
+
+
+  ArticleModel({this.author, this.title, this.url, this.content, this.urlToImage, this.description});
+
+}
